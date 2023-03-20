1566b5b86f9d1b3a6a08e65e008f1ff63f8823a8
Sebastian Kuzminsky <seb@highlab.com>
2023-03-19 18:28:35 -0600

    hm2_fw_eth: read udp packets on the hm2 port, print to console

--- firmware/hm2_fw_eth_w5500.c
+++ firmware/hm2_fw_eth_w5500.c
@@ -4,11 +4,6 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-/**
- * ----------------------------------------------------------------------------------------------------
- * Includes
- * ----------------------------------------------------------------------------------------------------
- */
 #include <stdio.h>
 
 #include "port_common.h"
@@ -16,63 +11,57 @@
 #include "wizchip_conf.h"
 #include "w5x00_spi.h"
 
-#include "httpServer.h"
-#include "web_page.h"
+#include "socket.h"
 
-/**
- * ----------------------------------------------------------------------------------------------------
- * Macros
- * ----------------------------------------------------------------------------------------------------
- */
+// #include "httpServer.h"
+// #include "web_page.h"
+
+
 /* Clock */
 #define PLL_SYS_KHZ (133 * 1000)
 
 /* Buffer */
-#define ETHERNET_BUF_MAX_SIZE (1024 * 2)
+// #define ETHERNET_BUF_MAX_SIZE (1024 * 2)
 
 /* Socket */
-#define HTTP_SOCKET_MAX_NUM 4
+// #define HTTP_SOCKET_MAX_NUM 4
 
-/**
- * ----------------------------------------------------------------------------------------------------
- * Variables
- * ----------------------------------------------------------------------------------------------------
- */
 /* Network */
-static wiz_NetInfo g_net_info =
-    {
-        .mac = {0x00, 0x08, 0xDC, 0x12, 0x34, 0x56}, // MAC address
-        .ip = {192, 168, 1, 121},                    // IP address
-        .sn = {255, 255, 255, 0},                    // Subnet Mask
-        .gw = {192, 168, 1, 1},                      // Gateway
-        .dns = {8, 8, 8, 8},                         // DNS server
-        .dhcp = NETINFO_STATIC                       // DHCP enable/disable
+static wiz_NetInfo g_net_info = {
+    .mac = {0x00, 0x08, 0xDC, 0x12, 0x34, 0x56}, // MAC address
+    .ip = {192, 168, 1, 121},                    // IP address
+    .sn = {255, 255, 255, 0},                    // Subnet Mask
+    .gw = {192, 168, 1, 1},                      // Gateway
+    .dns = {8, 8, 8, 8},                         // DNS server
+    .dhcp = NETINFO_STATIC                       // DHCP enable/disable
 };
 
 /* HTTP */
-static uint8_t g_http_send_buf[ETHERNET_BUF_MAX_SIZE] = {
-    0,
-};
-static uint8_t g_http_recv_buf[ETHERNET_BUF_MAX_SIZE] = {
-    0,
-};
-static uint8_t g_http_socket_num_list[HTTP_SOCKET_MAX_NUM] = {0, 1, 2, 3};
+// static uint8_t g_http_send_buf[ETHERNET_BUF_MAX_SIZE] = {
+    // 0,
+// };
+// static uint8_t g_http_recv_buf[ETHERNET_BUF_MAX_SIZE] = {
+    // 0,
+// };
+// static uint8_t g_http_socket_num_list[HTTP_SOCKET_MAX_NUM] = {0, 1, 2, 3};
 
-/**
- * ----------------------------------------------------------------------------------------------------
- * Functions
- * ----------------------------------------------------------------------------------------------------
- */
-/* Clock */
-static void set_clock_khz(void);
 
-/**
- * ----------------------------------------------------------------------------------------------------
- * Main
- * ----------------------------------------------------------------------------------------------------
- */
-int main()
-{
+static void set_clock_khz(void) {
+    // set a system clock frequency in khz
+    set_sys_clock_khz(PLL_SYS_KHZ, true);
+
+    // configure the specified clock
+    clock_configure(
+        clk_peri,
+        0,                                                // No glitchless mux
+        CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS, // System PLL on AUX mux
+        PLL_SYS_KHZ * 1000,                               // Input frequency
+        PLL_SYS_KHZ * 1000                                // Output (must be same as no divider)
+    );
+}
+
+
+int main() {
     stdio_init_all();
 
     uint const led_pin = 25;
@@ -100,9 +89,6 @@ int main()
     printf("hello world\n");
 
 
-    /* Initialize */
-    uint8_t i = 0;
-
     set_clock_khz();
 
     wizchip_spi_initialize();
@@ -114,42 +100,34 @@ int main()
 
     network_initialize(g_net_info);
 
-    httpServer_init(g_http_send_buf, g_http_recv_buf, HTTP_SOCKET_MAX_NUM, g_http_socket_num_list);
-
-    /* Get network information */
     print_network_information(g_net_info);
 
-    /* Register web page */
-    reg_httpServer_webContent("index.html", index_page);
 
-    /* Infinite loop */
-    while (1)
-    {
-        /* Run HTTP server */
-        for (i = 0; i < HTTP_SOCKET_MAX_NUM; i++)
-        {
-            httpServer_run(i);
+    // httpServer_init(g_http_send_buf, g_http_recv_buf, HTTP_SOCKET_MAX_NUM, g_http_socket_num_list);
+    // reg_httpServer_webContent("index.html", index_page);
+    // while (1) {
+    //     for (uint8_t i = 0; i < HTTP_SOCKET_MAX_NUM; i++) {
+    //         httpServer_run(i);
+    //     }
+    // }
+
+    int8_t sock = socket(0, Sn_MR_UDP, 27181, 0);
+    printf("socket %d\n", sock);
+
+    while (true) {
+        uint8_t packet[1024];
+        uint8_t addr[4];
+        uint16_t port;
+
+        int32_t r = recvfrom(0, packet, sizeof(packet), addr, &port);
+        printf("recvfrom %d (addr=%u.%u.%u.%u, port=%u)\n", r, addr[0], addr[1], addr[2], addr[3], port);
+
+        for (int i = 0; i < r; ++i) {
+            printf("0x%02x (%c) ", packet[i], packet[i]);
+            if (i % 8 == 7) {
+                printf("\n");
+            }
         }
+        printf("\n");
     }
-}
-
-/**
- * ----------------------------------------------------------------------------------------------------
- * Functions
- * ----------------------------------------------------------------------------------------------------
- */
-/* Clock */
-static void set_clock_khz(void)
-{
-    // set a system clock frequency in khz
-    set_sys_clock_khz(PLL_SYS_KHZ, true);
-
-    // configure the specified clock
-    clock_configure(
-        clk_peri,
-        0,                                                // No glitchless mux
-        CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS, // System PLL on AUX mux
-        PLL_SYS_KHZ * 1000,                               // Input frequency
-        PLL_SYS_KHZ * 1000                                // Output (must be same as no divider)
-    );
 }
