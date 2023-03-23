c848bf16839f66e4a2868774c3b96603017bc4c9
Sebastian Kuzminsky <seb@highlab.com>
2023-03-22 21:00:08 -0600

    build script improvements

--- build.sh
+++ build.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
 set -e
 
+
+PARALLEL=$(getconf _NPROCESSORS_ONLN)
+
+while [[ -v 1 ]]; do
+    case "$1" in
+
+        "--parallel" | "-j")
+            shift
+            PARALLEL="$1"
+            shift
+            ;;
+
+        "--verbose"|"-v")
+            VERBOSE="VERBOSE=1"
+            shift
+            ;;
+
+        *)
+            echo "unknown argument '$1'"
+            exit 1
+            ;;
+
+    esac
+done
+
+
 for PICO_BOARD in adafruit_feather_rp2040 wiznet_w5100s_evb_pico; do
     BUILD_DIR="build-${PICO_BOARD}"
+    MAKE_ARGS=(
+        -C"${BUILD_DIR}/firmware"
+        -j"${PARALLEL}"
+    )
+    if [[ -v VERBOSE ]]; then
+        MAKE_ARGS[${#MAKE_ARGS[@]}+1]="${VERBOSE}"
+    fi
+
     cmake -B "${BUILD_DIR}" -DPICO_BOARD="${PICO_BOARD}"
-    make -C"${BUILD_DIR}/firmware" -j$(getconf _NPROCESSORS_ONLN)
+    make "${MAKE_ARGS[@]}"
 done
